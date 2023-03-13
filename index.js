069270ab685e6e69cc0ba5e92cd0e487c03f6da4
Moustapha <tafaang44@gmail.com>
2023-03-13 14:52:07 +0100

    trql

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 
 var app = express();
-
-const port = 3000
+app.get("/toto", function(req,res){
+    res.send("salut toto");
+});
+const port = process.env.PORT || 3000;
 
 app.listen(port, function(){
 console.log('serveur listening on port:' +port)
